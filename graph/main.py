2e68dffac48e849bc64e2826bf581f2347def88f
Daniil Shishov <daniilshishov@MacBook-Pro-Daniil.local>
2023-05-24 05:33:45 +0300

    fixed imports

--- graph/main.py
+++ graph/main.py
@@ -1,7 +1,9 @@
-from graph.search import *
+import time
+
+from search import *
 
 if __name__ == '__main__':
-    _nrows = 80
+    _nrows = 30
     _df = ReadSource(_nrows, 'data/shipsData200.xlsx')
 
     print(_df.head())
@@ -12,5 +14,8 @@ if __name__ == '__main__':
     g = graphPreparation(_df)
     DrawGraph(g)
     maxK = 5
+    start = time.time()
     allBins = runShipsAllocation(g, maxK + 1, shipsQ, _df)
     print(allBins)
+    end = time.time()
+    print('\nThe program took {:.2f} s to compute. '.format(end - start))
